07434d6f6b8c10ece6a9841cb478870f4855c0e6
ashik0401 <ashik04am@gmail.com>
2025-07-16 23:15:17 +0600

    show latest added meals on top of the table

--- src/Pages/Admin/MealsTable.jsx
+++ src/Pages/Admin/MealsTable.jsx
@@ -60,8 +60,11 @@ const MealsTable = () => {
   if (isLoading) return <div className='flex items-center justify-center h-screen'><span className="loading loading-ring loading-md"></span></div>
 
   return (
-    <div className="p-4 mt-5 ">
-      <div className="flex gap-4 mb-2 ">
+    <div className="p-4 mt-5">
+      <div className="flex gap-4 mb-2">
+        <button className="btn btn-sm" onClick={() => handleSortChange('postTime')}>
+          Sort by Latest {sortBy === 'postTime' ? (order === 'asc' ? '↑' : '↓') : ''}
+        </button>
         <button className="btn btn-sm" onClick={() => handleSortChange('likes')}>
           Sort by Likes {sortBy === 'likes' ? (order === 'asc' ? '↑' : '↓') : ''}
         </button>
@@ -69,7 +72,7 @@ const MealsTable = () => {
           Sort by Reviews {sortBy === 'reviews_count' ? (order === 'asc' ? '↑' : '↓') : ''}
         </button>
       </div>
-      <div className="overflow-x-auto shadow-xl border border-gray-200 rounded-xl ">
+      <div className="overflow-x-auto shadow-xl border border-gray-200 rounded-xl">
         <table className="table w-full">
           <thead>
             <tr>
@@ -87,17 +90,19 @@ const MealsTable = () => {
                 <td className='text-center font-bold'>{meal.title}</td>
                 <td className='text-center'>{meal.likes}</td>
                 <td className='text-center'>{meal.reviews_count}</td>
-                <td className='text-center'><p>⭐ {
-                  (() => {
-                    const ratings = meal.ratings || {};
-                    const totalRatings = Object.values(ratings).reduce((a, b) => a + b, 0);
-                    if (totalRatings === 0) return 0;
-                    const totalScore = Object.entries(ratings).reduce(
-                      (acc, [star, count]) => acc + Number(star) * count, 0
-                    );
-                    return (totalScore / totalRatings).toFixed(1);
-                  })()
-                }</p></td>
+                <td className='text-center'>
+                  <p>⭐ {
+                    (() => {
+                      const ratings = meal.ratings || {}
+                      const totalRatings = Object.values(ratings).reduce((a, b) => a + b, 0)
+                      if (totalRatings === 0) return 0
+                      const totalScore = Object.entries(ratings).reduce(
+                        (acc, [star, count]) => acc + Number(star) * count, 0
+                      )
+                      return (totalScore / totalRatings).toFixed(1)
+                    })()
+                  }</p>
+                </td>
                 <td className='text-center'>{meal.distributorName.toUpperCase()}</td>
                 <td className="space-x-2 flex items-center justify-center md:text-center">
                   <button className="btn btn-xs btn-info" onClick={() => navigate(`/Meals/${meal._id}`)}>View</button>
